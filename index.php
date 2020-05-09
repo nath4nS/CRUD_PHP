896966faf9edc001e5b4baaa076fb415eb07510b
nath4ns <nathansousa360@gmail.com>
2020-05-09 18:06:58 -0300

    Correções

--- index.php
+++ index.php
@@ -3,37 +3,39 @@ include('layout/header_ext.php');
 ?>
 
 <p>&nbsp;</p>
-		<div class="col-6 offset-md-3">
-			<?php 
-				if(isset($_GET['msg']) && $_GET['msg'] != ''){
-			?>
-				<div class="alert alert-danger">
-					<?php echo $_GET['msg']; ?>
-				</div>
-			<?php } ?>
+<div class="container">
+	<div class="col-5 offset-md-3">
+		<?php 
+			if(isset($_GET['msg']) && $_GET['msg'] != ''){
+		?>
+		<div class="alert alert-danger">
+			<?php echo $_GET['msg']; ?>
+		</div>
+		<?php } ?>
 			<div class="card">
-					<div class="card-header">
-						<h2>Área restrita</h2>
-					</div>
-					<div class="card-body">
-						<form action="login/logar.php" method="post">
-							<div class="form-group">
-								<label>E-mail:</label>
-								<input type="text" name="email" 
+				<div class="card-header">
+					<h2 align="center">Administrativo</h2>
+				</div>
+				<div class="card-body">
+					<form action="login/logar.php" method="post">
+						<div class="form-group">
+							<label>E-mail:</label>
+						<input type="text" name="email" 
 									class="form-control" 
 									value="" required>
-							</div>
-							<div class="form-group">
-								<label>Senha:</label>
-								<input type="password" name="senha" class="form-control" value="" required>
-							</div>
-							<div class="form-group">
-								<button type="submit" class="btn btn-primary btn-block">Efetuar login</button>	
-							</div>
-						</form>
-					</div>
+						</div>
+						<div class="form-group">
+							<label>Senha:</label>
+							<input type="password" name="senha" class="form-control" value="" required>
+						</div>
+						<div class="form-group">
+							<button type="submit" class="btn btn-primary btn-block">Efetuar login</button>	
+						</div>
+					</form>
 				</div>
 			</div>
+	</div>
+</div>
 
 <?php 
 include('layout/footer.php'); 
--- layout/header_ext.php
+++ layout/header_ext.php
@@ -7,6 +7,7 @@ $path = "http://localhost/Nathan/oficina/";
 	<meta charset="utf-8">
 	<title>Dashboard - Minha Lanchonete</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo $path ?>css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo $path ?>css/estilo.css">
 	<link rel="stylesheet" type="text/css" href="https://kit-pro.fontawesome.com/releases/v5.12.1/css/pro.min.css">
 </head>
 <body>
